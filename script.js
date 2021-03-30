9c531de76217e140ae5e20a57d193760c7cd475f
EnderKingJ <jmbryzek@gmail.com>
2021-03-30 16:15:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function generate() {
+  window.open("https://cn1759620794-7vnsr30257.ibosscloud.com/syncCustomBranding/block_page/122385/9534/9534-block_page.html?fn="+ ('group')"&fp=6&bu=iboss.gay%2F&bc=Prohibited%20LGBTQ%20Content&ip=696.969.6.969&ibip=cn1759617723-vnsg11042.ibosscloud.com&user=Admin%20&ipm=cn1759617911-vnsg11308.ibosscloud.com)
+}
